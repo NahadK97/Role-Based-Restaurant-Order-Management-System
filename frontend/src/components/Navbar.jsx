a5f4d08c5c6481628067f2544a2548b7af8f4301
Nahad Bin Noushad <nahadnowshad14@gmail.com>
2025-07-08 12:53:06 +0530

    Name Change

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <header>
       <div className="container">
         <Link to="/">
-          <h1>Admin Window </h1>
+          <h1>Restaurant Management </h1>
         </Link>
         <nav>
           {user && (
